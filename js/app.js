6f278850089aa36a03922219127556179f381203
Adam Eivy <adam.eivy@disney.com>
2014-08-01 10:59:37 -0700

    Built from 4a7ee1a on branch dev

---
+++ js/app.js
@@ -0,0 +1,16 @@
+/*global $,Reveal*/
+Reveal.addEventListener( 'ready', function( /*event*/ ) {
+	'use strict';
+     // event.currentSlide, event.indexh, event.indexv
+     $('.lightbox_image').click(function(){
+     	//$('.slides').css({zoom:1});
+     	var $t = $(this),
+     		$img = $t.siblings().filter('.lightbox');
+     	$img.css({zoom:$('.slides').css('zoom')}).data('origin',$t);
+     	$('body').prepend($img);
+     });
+     $('.lightbox').click(function(){
+     	var $t = $(this);
+     	$t.data('origin').after($t);
+     });
+});
